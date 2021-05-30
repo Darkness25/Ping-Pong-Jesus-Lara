07af09a637aa51f5ea0add2710265a34e3e92d2b
JesusL <thedarkness2555@gmail.com>
2021-05-29 22:10:26 -0500

    Creacion de metodo play que se encarga de todo el funcionamiento del aplicativo. Creacion de la funcion de la bola, que contiene todos sus atributos. Se instancia la bola con sus respectivos atributos. Utilizamos map para que nos permita hacer una copia del arreglo bars. Asi ya no satura Agregamos el Keycode de la barra espaciadora para pausar el juego.

--- main.js
+++ main.js
@@ -42,6 +42,24 @@
 })();
 
 (function(){
+    self.Ball = function(x,y,radius,board){
+        console.log("constructor ball")
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.speed_y = 0;
+        this.speed_x = 3;
+        this.board = board;
+        this.direction = 1;
+        this.bounceAngle = 0;
+        this.maxBounceAngle = Math.PI / 12;
+        this.speed = 3;
+        board.ball = this;
+        this.kind = "circle";
+    }
+
+
+(function(){
     self.BoardView = function (canvas,board){
         this.canvas= canvas;
         this.canvas.width = board.width;
@@ -61,8 +79,20 @@
                 var el = this.board.elements[i];   
                 draw (this.ctx,el); 
             };
+        },
+
+        play: function (){
+            if (this.board.playing){
+                this.clean();
+                this.draw();
+                this.check_collisions();
+                this.board.ball.move();   
+            };
         }
     }
+
+
+    },
 
 
 
@@ -77,11 +107,41 @@
 
 })();
 
-    var board = new Board(800,400);
-    var canvas = document.getElementById('canvas');
-    var board_view = new BoardView(canvas,board);
-    var bar = new Bar(20,100,40,100,board);
-    var bar = new Bar(735,100,40,100,board);
+self.Ball.prototype = {
+    move: function (){
+        this.x += (this.speed_x*this.direction);
+        this.y += (this.speed_y*this.direction);
+    },
+    get width() {
+        return this.radius * 2;
+    },
+    get height() {
+        return this.radius * 2;
+    },
+    collision: function (bar){
+        var relativeIntersectY = (bar.y + (bar.height / 2)) - this.y;
+        var normalizedIntersectY = relativeIntersectY / (bar.height / 2);
+        this.bounceAngle = normalizedIntersectY * this.maxBounceAngle;
+
+        this.speed_y = this.speed * -Math.sin(this.bounceAngle);
+        this.speed_x = this.speed * Math.cos(this.bounceAngle);
+
+        if (this.x > (this.board.width / 2)) {
+            this.direction = -1;
+        } else {
+            this.direction = 1;
+        }
+    }
+}
+})();
+
+var board = new Board(800,400);
+console.log(board);
+var bar = new Bar (20,150,10,100,board)
+var bar2 = new Bar (770,150,10,100,board)
+var canvas = document.getElementById('canvas');
+var board_view = new BoardView(canvas, board);
+var ball = new Ball(350,200,7,board);
 
     window.requestAnimationFrame(controller);
 
@@ -104,13 +164,14 @@
         } 
     });
 
+    board_view.draw();
+    window.requestAnimationFrame(controller);
 
 //self.addEventListener("load", main);
 
 function controller(){
     
     window.requestAnimationFrame(controller);
-    board_view.draw();
-    board_view.clean();
+    board_view.play();
     
 }
